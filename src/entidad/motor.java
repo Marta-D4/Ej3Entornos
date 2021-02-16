01eef6349786647b6c3094702d33a7f935d454cb
MARTA DIAZ ESCALONA <22048938@alumnos.uem.es>
2021-02-16 10:54:56 +0100

    clase motor en la rama motor

---
+++ src/entidad/motor.java
@@ -0,0 +1,37 @@
+package entidad;
+
+public class motor {
+
+	private int id;
+	private String tipoMotor;
+	private int potencia;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTipoMotor() {
+		return tipoMotor;
+	}
+	public void setTipoMotor(String tipoMotor) {
+		this.tipoMotor = tipoMotor;
+	}
+	public int getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+	
+	public motor() {
+		super();
+	}
+	public motor(int id, String tipoMotor, int potencia) {
+		super();
+		this.id = id;
+		this.tipoMotor = tipoMotor;
+		this.potencia = potencia;
+	}
+	
+}
